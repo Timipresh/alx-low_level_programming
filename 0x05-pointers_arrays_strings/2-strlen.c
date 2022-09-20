e80e2a4f5e8c2964e3cac760f8e2e7cbadd23b03
AMADITIMI PRECIOUS <timikekuru@gmail.com>
2022-09-20 15:01:01 -0700

    made corrections

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+/**
+ *
+ * _strlen - returns the length of a string
+ * @s : s is a string variable
+ * Description: function returns the length of a string
+ */
+
+ int _strlen(char *s)
+{
+	return strlen(s);
+}
