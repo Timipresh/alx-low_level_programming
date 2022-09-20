697a8a7cce91aaa381ee35359feeaf5d65a810e2
AMADITIMI PRECIOUS <timikekuru@gmail.com>
2022-09-20 13:53:04 -0700

    made corrections

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,7 @@
 /**
- * Pointer Demonstration
  * reset_to_98 - This function deferences a pointer
  * @n: n is a pointer variable
- * Description: function that takes a pointer
- * to an int as parameter and updates the value
- * it points to to 98
+ * Description: Pointer demonstration
  */
 
 void reset_to_98(int *n)
