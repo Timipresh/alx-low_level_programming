fc2630bdc91391df43875e2ebff6330ccbf2e42c
AMADITIMI PRECIOUS <timikekuru@gmail.com>
2022-09-21 08:15:47 -0700

    new file

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+/**
+ *_strcat - contenates strings
+ *@dest : string variable
+ *@src: string variable
+ *Return: returns a char
+ */
+
+char *_strcat(char *dest, char *src)
+{	     
+       	dest = strcat(dest, src);
+	return (dest);		       
+}
